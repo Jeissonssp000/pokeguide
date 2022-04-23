13b2525316c0878e9ca1657fb0d9e2d75de20911
jeissonssp <jeissonssp@gmail.com>
2022-04-22 22:47:20 -0500

    app name, logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { InfoContext } from "../context/InfoContext";
+import logo from "../assets/logo.png";
 
 const Header = () => {
   const { search, searchHandler } = InfoContext();
   return (
     <header className="header-container">
-      <img
-        src="https://raw.githubusercontent.com/Mondal10/Pokedex/master/public/assets/logo-128.png"
-        alt="logo"
-        height="40"
-      />
+      <img src={logo} alt="logo" height="40" />
       <label className="search">
         <input
           value={search}
